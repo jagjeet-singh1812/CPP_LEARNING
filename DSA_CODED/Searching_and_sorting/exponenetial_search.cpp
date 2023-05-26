3654c560993e2a62b02edb62b6510bbe66b65de5
jagjeet <sappaljagjeet@gmail.com>
2023-05-27 01:31:32 +0530

    updated

---
+++ DSA_CODED/Searching_and_sorting/exponenetial_search.cpp
@@ -0,0 +1,92 @@
+//also know as doubling search galloping/stazile/search
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+//you are given a sorted array and find some elment but its time complexity is optimized of binary search
+// applied when array size is too large
+// its time complexity is O(log i) where i is stopping point 
+// application
+// search in infinite array
+// better in unbounded array
+
+// -------------------------------------------------------------------------------------------------
+// https://leetcode.com/discuss/interview-experience/1979273/infinite-sorted-array refer thhis
+//----------------------------------------------------------------------------------------------------
+
+int binary_search_my(vector<int> &v, int low, int high, int key)
+{ // time complexity-->log(n) where n is size of array
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2; // to avoid overflow
+        if (v[mid] == key)
+            return mid;
+        else if (key < v[mid])
+        {
+            high = mid - 1;
+        }
+        else if (key > v[mid])
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int exponential_search(vector<int>&arr,int key){
+    if(arr[0]==key){
+        return 0;
+    }
+    int i=1;
+    int n=arr.size()-1;
+    while(arr[i]<=key){
+        i=i<<1;
+    }
+    return binary_search_my(arr,i/2,min(i,n-1),key);
+}
+
+int exponential_search_infinite_array(vector<int>&arr,int key){
+    if(arr[0]==key){
+        return 0;
+    }
+    int low=0;
+    int high=1;
+    int n=arr.size()-1;
+    while(arr[high]<=key){
+        low=high;
+        high=high*2;
+    }
+    return binary_search_my(arr,low,high,key);
+}
+void solve(){
+ll n;
+cin>>n;
+vector<int>v(n);
+for(int i=0;i<n;i++){
+cin>>v[i];
+}
+int target;
+cin>>target;
+cout<<"index is "<<exponential_search(v,target);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+
+#endif
+    int t;
+    cin>>t;
+
+    while(t-- >0)
+    {
+        solve();
+    }
+    return 0;
+}
--- DSA_CODED/Searching_and_sorting/find_k_closest.cpp
+++ DSA_CODED/Searching_and_sorting/find_k_closest.cpp
@@ -1,7 +1,104 @@
-// https://leetcode.com/problems/find-k-closest-elements/
+// https://leetcode.com/problems/find-k-closest-elements
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
+
+//naive approach
+class Solution1
+ {
+public:
+
+static bool comp(pair<int,int>&a,pair<int,int>&b){
+    return a.second<b.second;
+}
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int>ans;
+        vector<pair<int,int>>diff(arr.size());
+        for(int i=0;i<arr.size();i++){
+            diff[i].first=arr[i];
+            diff[i].second=abs(arr[i]-x);
+        }
+         stable_sort(begin(diff),end(diff),[x](const auto a, const auto b){
+            return a.second<b.second;   // sort by distance from x
+        });
+         int temp=0;
+         for(auto j:diff){
+             if(temp<k)ans.push_back(j.first);
+             temp++;
+         }
+         sort(ans.begin(),ans.end());
+         return ans;
+    }
+};//
+
+//two pointer approach soln
+class Solution2 {
+public:
+// O(n-k) approach
+
+vector<int>twopointer(vector<int>& a, int k, int x){
+    int h=a.size()-1;
+    int l=0;
+    while(h-l>=k){
+        if(x-a[l]>a[h]-x) l++; //go to right
+        else h--;
+    }
+    return vector<int>(a.begin()+l,a.begin()+h+1);
+}
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+      return twopointer(arr,k,x);
+    }
+};
+
+// bs search approach
+
+class Solution {
+public:
+    int lowerbound(vector<int>& arr, int x) {
+        int l = 0;
+        int h = arr.size() - 1;
+        int ans = h;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                h = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        if (k >= arr.size()) {
+            return arr;
+        }
+
+        vector<int> result;
+        int h = lowerbound(arr, x);
+        int l = h - 1;
+
+        while (k > 0) {
+            if (l < 0) {
+                h++;
+            }
+            else if (h >= arr.size()) {
+                l--;
+            }
+            else if (x - arr[l] > arr[h] - x) {
+                h++;
+            }
+            else {
+                l--;
+            }
+            k--;
+        }
+
+        return vector<int>(arr.begin() + l + 1, arr.begin() + h);
+    }
+};
 
 
 int main()
@@ -13,13 +110,6 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     freopen("error.txt", "w", stderr);
-
 #endif
-    int t;
-    cin>>t;
-    while(t-- >0)
-    {
-      
-    }
     return 0;
 }
